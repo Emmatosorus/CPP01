a0577bc80b45d1870ea760caa74ce037e3ecdbd7
Emmatosorus <emma@bouteyres.com>
2024-07-09 16:00:57 +0200

    Add ✨ : Functionnal ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 #include <fstream>
 
+void	replace(std::string *text, std::string s1, std::string s2)
+{
+	size_t i = 0;
+	size_t pos;
+	size_t size = text->size();
+	while (i < size)
+	{
+		pos = text->find(s1);
+		if (pos == std::string::npos)
+			return ;
+		text->erase(pos, s1.length());
+		text->insert(pos, s2);
+		i += s2.length() + 1;
+	}
+}
+
+int	get_file(std::string *text, std::string file)
+{
+	std::ifstream	ifs(file.c_str());
+	if (!ifs)
+	{
+		std::cout << "Please enter valid file" << std::endl;
+		return (1);
+	}
+	*text += ifs.get();
+	while (ifs.good())
+		*text += ifs.get();
+	ifs.close();
+	(*text)[text->length() - 1] = 0;
+	return (0);
+}
+
 int main(int ac, char **av)
 {
 	if (ac < 4 || ac > 4)
@@ -9,17 +41,17 @@ int main(int ac, char **av)
 		return (1);
 	}
 	std::string		file = av[1];
-	std::ifstream	ifs(file.c_str());
+	std::string		s1 = av[2];
+	std::string		s2 = av[3];
 	std::string		text;
 
-	text += ifs.get();
-	while (ifs.good())
-		text += ifs.get();
-	ifs.close();
-	std::cout << text;
+	if (get_file(&text, file) != 0)
+		return (1);
+	replace(&text, s1, s2);
+
 	file += ".replace";
 	std::ofstream new_file(file.c_str());
-	new_file << "It worked!";
+	new_file << text.substr(0, text.length() - 1);
 	new_file.close();
 	return (0);
 }
